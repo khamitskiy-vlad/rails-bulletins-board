9ad5b7a667c5adb5c6f06c03f820d8168570bda4
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-31 11:30:02 +0300

    helper for omniauth tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+OmniAuth.config.test_mode = true
 
 module ActiveSupport
   class TestCase
@@ -13,5 +14,30 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  def sign_in(user, options = {})
+    auth_hash = {
+      provider: 'github',
+      uid: '12345',
+      info: {
+        email: user.email,
+        name: user.name
+      }
+    }
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash::InfoHash.new(auth_hash)
+
+    get callback_auth_url('github')
+  end
+
+  def signed_in?
+    session[:user_id].present? && current_user.present?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
