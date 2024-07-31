8524a3536ce1e17c96dc710f77d798482d6af552
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-31 11:29:11 +0300

    def from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+
+  def self.from_omniauth(omniauth_params)
+    omniauth_email = omniauth_params.info.email
+
+    user = User.find_or_initialize_by(email: omniauth_email)
+    user.name = omniauth_params.info.name
+    user.save
+    user
+  end
 end
