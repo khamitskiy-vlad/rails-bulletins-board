9c43040d56279e6782c592637916303f09b17176
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-31 11:29:34 +0300

    scoped in web module

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
-  root 'home#index'
+  scope module: :web do
+    root 'home#index'
+    resources :categories
+    resources :users
+  end
+  
+  scope module: :web do
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+    get 'auth/failure', to: 'auth#failure', as: :failure_auth
+    delete 'sign_out', to: 'auth#destroy', as: :destroy_session
+  end
 end
