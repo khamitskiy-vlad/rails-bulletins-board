52531612f01624f7695206d9e3a828a207406353
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-08-02 00:42:09 +0300

    install pundit for bulletins

--- app/controllers/web/bulletins_controller.rb
+++ app/controllers/web/bulletins_controller.rb
@@ -1,31 +1,37 @@
 # frozen_string_literal: true
 
 class Web::BulletinsController < Web::ApplicationController
-  before_action :authenticate_user!, only: %i[new edit create update destroy]
   include Pagy::Backend
+
+  before_action :authenticate_user!, only: %i[new edit create update destroy]
 
   def index
     @search_query = set_bulletins
     @pagy, @bulletins = pagy(@search_query.result
                                           .order(created_at: :desc)
                                           .includes(:creator, :category))
+    authorize @bulletins
   end
 
   def show
     @bulletin = set_bulletin
+    authorize @bulletin
     @creator = @bulletin.creator
   end
 
   def new
     @bulletin = Bulletin.new
+    authorize @bulletin
   end
 
   def edit
     @bulletin = set_bulletin
+    authorize @bulletin
   end
 
   def create
     @bulletin = current_user.bulletins.build(bulletin_params)
+    authorize @bulletin
 
     if @bulletin.save
       redirect_to bulletin_url(@bulletin), notice: 'Bulletin was successfully created.'
@@ -36,6 +42,7 @@ class Web::BulletinsController < Web::ApplicationController
 
   def update
     @bulletin = set_bulletin
+    authorize @bulletin
 
     if user_verified? 
       @bulletin.update(bulletin_params)
@@ -47,6 +54,7 @@ class Web::BulletinsController < Web::ApplicationController
 
   def destroy
     @bulletin = set_bulletin
+    authorize @bulletin
 
     if user_verified?
       @bulletin.destroy!
@@ -63,7 +71,7 @@ class Web::BulletinsController < Web::ApplicationController
   end
 
   def set_bulletin
-    @bulletin = Bulletin.find(params[:id])
+    Bulletin.find(params[:id])
   end
   
   def set_bulletins
