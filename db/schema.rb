25998f20d8b1f0661073d2e1dbf37e54d2412c26
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-08-01 21:41:48 +0300

    changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_31_121400) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_181743) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,12 +42,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_31_121400) do
   create_table "bulletins", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
-    t.integer "user_id", null: false
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
     t.index ["category_id"], name: "index_bulletins_on_category_id"
-    t.index ["user_id"], name: "index_bulletins_on_user_id"
+    t.index ["creator_id"], name: "index_bulletins_on_creator_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -62,11 +62,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_31_121400) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bulletins", "categories"
-  add_foreign_key "bulletins", "users"
+  add_foreign_key "bulletins", "users", column: "creator_id"
 end
