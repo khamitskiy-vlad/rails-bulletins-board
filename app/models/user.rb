d8920616ac4fd9bf293ae6dcb24d09a6a8ee92d8
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-08-01 21:42:35 +0300

    cretor_id for bulletins

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :bulletins, dependent: :destroy
+  has_many :bulletins, foreign_key: 'creator_id', inverse_of: :creator, dependent: :destroy
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
 
