cb1716e4cf38c5de052729f1f13d187d0db55bf3
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-08-12 22:57:17 +0300

    added profiles and changed creator for bulletins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_08_204022) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_12_182854) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_08_204022) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "creator_id"
+    t.integer "creator_id", null: false
     t.index ["category_id"], name: "index_bulletins_on_category_id"
     t.index ["creator_id"], name: "index_bulletins_on_creator_id"
   end
@@ -56,6 +56,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_08_204022) do
     t.datetime "updated_at", null: false
     t.integer "bulletins_count"
     t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -72,5 +80,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_08_204022) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bulletins", "categories"
-  add_foreign_key "bulletins", "users", column: "creator_id"
+  add_foreign_key "bulletins", "profiles", column: "creator_id"
+  add_foreign_key "profiles", "users"
 end
