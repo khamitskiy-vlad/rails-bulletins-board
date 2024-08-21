73cdd174cd7d340553209d8ed0390df7ed61e0ca
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-08-21 21:19:10 +0300

    removed resource user helper

--- app/controllers/web/users/application_controller.rb
+++ app/controllers/web/users/application_controller.rb
@@ -2,10 +2,5 @@
 
 module Web
   class Users::ApplicationController < ApplicationController
-    helper_method :resource_user
-
-    def resource_user
-      @resource_user ||= User.find params[:user_id]
-    end
   end
 end
